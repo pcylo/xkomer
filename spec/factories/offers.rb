80ec3ff73d98512c1096b7c01fc5d745e7a635de
pcylo <pawel@firma.xin.pl>
2017-05-06 20:43:23 +0200

    Remove unused code from offer factory class

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -5,9 +5,6 @@ FactoryGirl.define do
     new_price    "499,00 zł"
     discount     "80 zł"
     product_code Random.rand(99999)
-    # tags      [Faker::Lorem.word, Faker::Lorem.words(2), Faker::Lorem.words(3)]
-    # quantity  Random.rand(200)
-    # image_url "https://cdn.x-kom.pl/i/img/promotions/hot-shot,,hs_xxx.PNG"
 
     initialize_with { new(attributes) }
     to_create { |instance| OfferRepository.new.create(instance) }
